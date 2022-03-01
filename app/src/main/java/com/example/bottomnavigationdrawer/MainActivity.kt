4b363523a1017e6a9291d6be1c5dbd155b79787c
fuadh <tanimulislam.fuad@gmail.com>
2022-03-01 14:34:52 +0600

    complete

--- app/src/main/java/com/example/bottomnavigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationdrawer/MainActivity.kt
@@ -2,15 +2,19 @@ package com.example.bottomnavigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.bottomnavigationdrawer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        binding.buttomNavigation.setOnItemSelectedListener {
+            Toast.makeText(this, "Clicked : $it", Toast.LENGTH_SHORT).show()
+            return@setOnItemSelectedListener true
+        }
     }
 }
